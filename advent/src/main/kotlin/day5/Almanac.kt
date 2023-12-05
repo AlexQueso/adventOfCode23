fed6cbdcb4a421c9f7f915317a4048335881619a
Alex Quesada <a.quemen@gmail.com>
2023-12-05 17:02:04 +0100

    day 5 part 2

--- advent/src/main/kotlin/day5/Almanac.kt
+++ advent/src/main/kotlin/day5/Almanac.kt
@@ -20,19 +20,39 @@ class Almanac (
     val lightToTemperature = Map()
     val temperatureToHumidity = Map()
     val humidityToLocation = Map()
-    val seedLocations = mutableListOf<Long>()
+    private val seedLocations = mutableListOf<Long>()
 
     fun lowestLocation(): Long {
         seeds.forEach {
-            val soil = seedToSoilMap.map(it)
-            val fertilizer = soilToFertilizer.map(soil)
-            val water = fertilizerToWater.map(fertilizer)
-            val light = waterToLight.map(water)
-            val temperature = lightToTemperature.map(light)
-            val humidity = temperatureToHumidity.map(temperature)
-            seedLocations.add(humidityToLocation.map(humidity))
+            seedLocations.add(getLocationValue(it))
         }
         return minimum(seedLocations)
+    }
+
+    private fun getLocationValue(seed: Long) : Long {
+        val soil = seedToSoilMap.map(seed)
+        val fertilizer = soilToFertilizer.map(soil)
+        val water = fertilizerToWater.map(fertilizer)
+        val light = waterToLight.map(water)
+        val temperature = lightToTemperature.map(light)
+        val humidity = temperatureToHumidity.map(temperature)
+        return  humidityToLocation.map(humidity)
+    }
+
+    fun lowestLocationWithSeedRange(): Long {
+        var seedRangeIndex = 0;
+        var minimum = Long.MAX_VALUE
+        while (seedRangeIndex < seeds.size) {
+            for (seedNumber in seeds[seedRangeIndex] until seeds[seedRangeIndex] + seeds[seedRangeIndex+1]){
+                val location = getLocationValue(seedNumber)
+                if (location<minimum){
+                    minimum = location
+                    println("new minimum location: $minimum")
+                }
+            }
+            seedRangeIndex += 2
+        }
+        return minimum
     }
 
     private fun minimum(list: List<Long>): Long {
@@ -44,6 +64,8 @@ class Almanac (
         }
         return minimum
     }
+
+
 }
 
 
@@ -51,13 +73,7 @@ class AlmanacRecord(private val record: List<String>) {
     private val almanac = Almanac()
     fun getAlmanac(): Almanac {
         setSeeds()
-        setMap(SEED, almanac.seedToSoilMap)
-        setMap(SOIL, almanac.soilToFertilizer)
-        setMap(FERTILIZER, almanac.fertilizerToWater)
-        setMap(WATER, almanac.waterToLight)
-        setMap(LIGHT, almanac.lightToTemperature)
-        setMap(TEMPERATURE, almanac.temperatureToHumidity)
-        setMap(HUMIDITY, almanac.humidityToLocation)
+        setMaps()
         return almanac
     }
 
@@ -68,6 +84,16 @@ class AlmanacRecord(private val record: List<String>) {
             .forEach{
                 almanac.seeds.add(it.toLong())
             }
+    }
+
+    private fun setMaps() {
+        setMap(SEED, almanac.seedToSoilMap)
+        setMap(SOIL, almanac.soilToFertilizer)
+        setMap(FERTILIZER, almanac.fertilizerToWater)
+        setMap(WATER, almanac.waterToLight)
+        setMap(LIGHT, almanac.lightToTemperature)
+        setMap(TEMPERATURE, almanac.temperatureToHumidity)
+        setMap(HUMIDITY, almanac.humidityToLocation)
     }
 
     private fun setMap(mapType: String, map: Map){
@@ -85,5 +111,4 @@ class AlmanacRecord(private val record: List<String>) {
             mapIndex++
         }
     }
-
 }
--- advent/src/main/kotlin/day5/Day5.kt
+++ advent/src/main/kotlin/day5/Day5.kt
@@ -8,9 +8,14 @@ const val DAY_NUMBER = 5
 fun main() {
     println("part 1(example): " + calculateLowestLocation(getExample1FileFromDayNumber(DAY_NUMBER)))
     println("part 1: " + calculateLowestLocation(getInputFileFromDayNumber(DAY_NUMBER)))
-    println("part 2: ")
+    println("part 2(example): " + calculateLowestLocationWithSeedRange(getExample1FileFromDayNumber(DAY_NUMBER)))
+    println("part 2: " + calculateLowestLocationWithSeedRange(getInputFileFromDayNumber(DAY_NUMBER)))
 }
 
 fun calculateLowestLocation(input: File): Long {
     return AlmanacRecord(input.readLines()).getAlmanac().lowestLocation()
+}
+
+fun calculateLowestLocationWithSeedRange(input: File): Long {
+    return AlmanacRecord(input.readLines()).getAlmanac().lowestLocationWithSeedRange()
 }
