155c1857d22b2254f3ac568bb2088e74e0ed1c7c
Alex Quesada <a.quemen@gmail.com>
2023-12-06 13:29:58 +0100

    day 6 solution

---
+++ advent/src/main/kotlin/day6/Day6.kt
@@ -0,0 +1,21 @@
+package day6
+
+import utils.getExample1FileFromDayNumber
+import utils.getInputFileFromDayNumber
+import java.io.File
+
+const val DAY_NUMBER = 6
+fun main() {
+    println("part 1(example): " + numberOfWinningPossibilities(getExample1FileFromDayNumber(DAY_NUMBER)))
+    println("part 1: " + numberOfWinningPossibilities(getInputFileFromDayNumber(DAY_NUMBER)))
+    println("part 2(example): " + numberOfWinningPossibilitiesNoKerning(getExample1FileFromDayNumber(DAY_NUMBER)))
+    println("part 2: " + numberOfWinningPossibilitiesNoKerning(getInputFileFromDayNumber(DAY_NUMBER)))
+}
+
+fun numberOfWinningPossibilitiesNoKerning(input: File): Long {
+    return CompetitionRecord(input.readLines()).getCompetitionNoKerning().numberOfWinningPossibilities()
+}
+
+fun numberOfWinningPossibilities(input: File): Long {
+    return CompetitionRecord(input.readLines()).getCompetition().numberOfWinningPossibilities()
+}
