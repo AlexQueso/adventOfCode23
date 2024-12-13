23d54e463958363ccff1336b54b395383c4f4af9
Alex Quesada <alejandro.quesada@allianzdirect.es>
2024-12-13 09:33:17 +0100

    day 1, 2024

--- advent/src/main/kotlin/twentyFour/day01/Day1.kt
+++ advent/src/main/kotlin/twentyFour/day01/Day1.kt
@@ -1,6 +1,21 @@
 package twentyFour.day01
 
-fun main(){
+import java.io.File
+import utils.getExample1FileFromDayNumber
+import utils.getInputFileFromDayNumber
 
+const val DAY_NUMBER = "01"
+fun main() {
+    println("part 1(example): " + totalDistanceBetweenLists(getExample1FileFromDayNumber(DAY_NUMBER, "2024")))
+    println("part 1: " + totalDistanceBetweenLists(getInputFileFromDayNumber(DAY_NUMBER, "2024")))
+    println("part 2(example): " + totalSimilarities(getExample1FileFromDayNumber(DAY_NUMBER, "2024")))
+    println("part 2: " + totalSimilarities(getInputFileFromDayNumber(DAY_NUMBER, "2024")))
 }
 
+fun totalDistanceBetweenLists(input: File) = LocationRecord(input.readLines())
+    .getLocations()
+    .totalDistanceBetweenLists()
+
+fun totalSimilarities(input: File) = LocationRecord(input.readLines())
+    .getLocations()
+    .totalSimilarities()
---
+++ advent/src/main/kotlin/twentyFour/day01/Locations.kt
@@ -0,0 +1,43 @@
+package twentyFour.day01
+
+class Locations(val locationList: List<MutableList<Int>>) {
+
+    private val similarities = mutableMapOf<Int, Int>()
+
+    fun totalDistanceBetweenLists(): Int {
+        val sortedLocations = locationList.map { it.sorted() }
+
+        return sortedLocations[0].indices.sumOf { i ->
+            getPairDistance(sortedLocations[0][i], sortedLocations[1][i])
+        }
+    }
+
+    private fun getPairDistance(location1: Int, location2: Int): Int {
+        return Math.abs(location1 - location2)
+    }
+
+    fun totalSimilarities(): Int {
+        var totalSimilarities = 0
+        locationList[0].forEach { location ->
+            totalSimilarities += similarities.getOrPut(location) { calculateTotalAppearance(location) }
+        }
+        return totalSimilarities
+    }
+
+    private fun calculateTotalAppearance(location: Int) = (locationList[1].filter { it == location }.size) * location
+}
+
+class LocationRecord(private val record: List<String>) {
+
+
+    private val locations = mutableListOf(mutableListOf<Int>(), mutableListOf())
+
+    fun getLocations(): Locations {
+        record.forEach {
+            val locationsInLine = it.split("   ")
+            locations[0].add(locationsInLine[0].toInt())
+            locations[1].add(locationsInLine[1].toInt())
+        }
+        return Locations(locations)
+    }
+}
